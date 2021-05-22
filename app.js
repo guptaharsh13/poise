dcb76f7be8793201680ac2413a545726e99eb46f
Harsh Gupta <hg242322@gmail.com>
2021-05-22 20:04:48 +0530

    images lag

--- app.js
+++ app.js
@@ -45,3 +45,38 @@ function paginator()
    }
    document.querySelector(".paginator a:nth-child(" + (i+1) + ")").classList.add("active")
 }
+
+// let about_img = document.querySelector(".about .about-img")
+// const vh = window.innerHeight
+// const aboutImg_offset = about_img.offsetTop - vh
+// const parallax_effect = about_img.getAttribute("parallax-effect")
+
+// let x = window.matchMedia("(min-width: 590px)")
+
+// if(x.matches)
+// {
+//    window.onscroll = () => {
+//       if(window.scrollY > aboutImg_offset)
+//          {
+//             let temp = (window.scrollY - aboutImg_offset - vh) * parallax_effect
+//             about_img.style.backgroundPositionY = temp + "px" 
+//          }
+//    }
+// }
+
+// let scrolling = false
+
+// window.onscroll = () => {
+//    scrolling = true
+// }
+
+// setInterval(() => {
+//    if(scrolling)
+//    {
+//       if(window.scrollY > aboutImg_offset)
+//       {
+//          let temp = (window.scrollY - aboutImg_offset - vh) * parallax_effect
+//         about_img.style.backgroundPositionY = temp + "px" 
+//       }
+//    }
+// }, 1);
